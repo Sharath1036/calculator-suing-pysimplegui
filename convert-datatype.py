e0b7e518d127f142c1f9c704ff379bb11186f801
Sharath Pai <83855796+Sharath1036@users.noreply.github.com>
2023-08-04 11:57:42 +0530

    Delete convert-datatype.py

--- convert-datatype.py
+++
@@ -1,9 +0,0 @@
-else :
-            Answer = str("%0.7f" %(eval(Equal)))                         
-            if '.0' in Answer:
-                Answer = str(int(float(Answer)))                         
-        form.FindElement('input').Update(Answer)                         
-        Equal = Answer
-
-    elif button is 'Quit'  or button is None:                            
-        break
